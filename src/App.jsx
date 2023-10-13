a0cc83cc3e50c45c36419a88a3bd4ad5792b0e7b
alexanderley <112874444+alexanderley@users.noreply.github.com>
2023-10-13 10:54:08 +0200

    setting up the design

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Todos Applikation</h1>
+      <div className="stepContainer">
+        <div className="step">
+          <h3>Todo</h3>
+          <div className="taskContainer">
+            <div className="task">Changing styles</div>
+            <div className="task">Addapt fonts</div>
+          </div>
+        </div>
+        <div className="step">
+          <h3>In Progress</h3>
+          <div className="taskContainer">
+            <div className="task">SEO</div>
+            <div className="task">Research</div>
+            <div className="task">Progress</div>
+            <div className="task">Performance Test</div>
+            <div className="task">Client test</div>
+            <div className="task">Waiting aprovement</div>
+          </div>
+        </div>
+        <div className="step">
+          <h3>Done</h3>
+          <div className="taskContainer">
+            <div className="task">Design</div>
+            <div className="task">Figma files</div>
+            <div className="task">Setting Boards</div>
+          </div>
+        </div>
+        <div className="step">
+          <h3>Approved</h3>
+          <div className="taskContainer">
+            <div className="task">Concept creation</div>
+            <div className="task">Layouts</div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
