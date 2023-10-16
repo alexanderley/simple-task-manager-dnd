4d8c48d9d572b77ff8e64018482d48611030989f
alexanderley <112874444+alexanderley@users.noreply.github.com>
2023-10-16 23:00:45 +0200

    added dnd functionality

--- src/App.jsx
+++ src/App.jsx
@@ -57,74 +57,113 @@ import "./App.css";
 
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-function App() {
-  const [count, setCount] = useState(0);
+const DATA = [
+  {
+    id: "0e2f0db1-5457-46b0-949e-8032d2f9997a",
+    step: "Todo",
+    items: [
+      { id: "26fd50b3-3841-496e-8b32-73636f6f4197", name: "Change Fonts" },
+      { id: "b0ee9d50-d0a6-46f8-96e3-7f3f0f9a2525", name: "Adapt CSS" },
+      {
+        id: "cafebeef-1234-5678-9abc-def012345678",
+        name: "Customize Layout",
+      },
+    ],
+  },
+  {
+    id: "487f68b4-1746-438c-920e-d67b7df46247",
+    step: "In Progress",
+    items: [
+      { id: "deadbeef-abcd-1234-5678-badcoffeebadc0", name: "Check Changes" },
+      {
+        id: "25daffdc-aae0-4d73-bd31-43f73101e7c05",
+        name: "Create Styles",
+      },
+    ],
+  },
+  {
+    id: "abcdef12-34ab-cdef-5678-901234567890",
+    step: "Done",
+    items: [
+      {
+        id: "95ee6a5d-f927-4579-8c15-2b4eb86210ae",
+        name: "Designing Data",
+      },
+      { id: "5bee94eb-6bde-4411-b438-1c37fa6af364", name: "Atomic Habits" },
+    ],
+  },
+  {
+    id: "98765432-abcdef-1234-abcd-567812345abc",
+    step: "Approved",
+    items: [
+      {
+        id: "960cbbcf-89a0-4d79-aa8e-56abbc15eacc",
+        name: "Approve with customer",
+      },
+      { id: "d3edf796-6449-4931-a777-ff66965a025b", name: "Final Call" },
+    ],
+  },
+];
 
-  // Define an array of tasks
-  const tasks = [
-    {
-      id: "0e2f0db1-5457-46b0-949e-8032d2f9997a",
-      step: "Todo",
-      items: [
-        { id: "26fd50b3-3841-496e-8b32-73636f6f4197", name: "Change Fonts" },
-        { id: "b0ee9d50-d0a6-46f8-96e3-7f3f0f9a2525", name: "Adapt CSS" },
-        {
-          id: "cafebeef-1234-5678-9abc-def012345678",
-          name: "Customize Layout",
-        },
-      ],
-    },
-    {
-      id: "487f68b4-1746-438c-920e-d67b7df46247",
-      step: "In Progress",
-      items: [
-        { id: "deadbeef-abcd-1234-5678-badcoffeebadc0", name: "Check Changes" },
-        {
-          id: "25daffdc-aae0-4d73-bd31-43f73101e7c05",
-          name: "Create Styles",
-        },
-      ],
-    },
-    {
-      id: "abcdef12-34ab-cdef-5678-901234567890",
-      step: "Done",
-      items: [
-        {
-          id: "95ee6a5d-f927-4579-8c15-2b4eb86210ae",
-          name: "Designing Data",
-        },
-        { id: "5bee94eb-6bde-4411-b438-1c37fa6af364", name: "Atomic Habits" },
-      ],
-    },
-    {
-      id: "98765432-abcdef-1234-abcd-567812345abc",
-      step: "Approved",
-      items: [
-        {
-          id: "960cbbcf-89a0-4d79-aa8e-56abbc15eacc",
-          name: "Approve with customer",
-        },
-        { id: "d3edf796-6449-4931-a777-ff66965a025b", name: "Final Call" },
-      ],
-    },
-  ];
+function App() {
+  const [tasks, setTasks] = useState(DATA);
+
+  const handleDragDrop = (results) => {
+    console.log("Results: ", results);
+
+    const { source, destination, type } = results;
+  };
 
   return (
     <>
       <h1>Todos Application</h1>
       <div className="stepContainer">
-        {tasks.map((taskGroup) => (
-          <div className="step" key={taskGroup.id}>
-            <h3>{taskGroup.step}</h3>
-            <div className="taskContainer">
-              {taskGroup.items.map((task, taskIndex) => (
-                <div className="task" key={task.id}>
-                  {task.name}
-                </div>
-              ))}
+        <DragDropContext onDragEnd={handleDragDrop}>
+          <Droppable droppableId="ROOT" type="group">
+            {(provided) => (
+              <div {...provided.droppableProps} ref={provided.innerRef}>
+                {tasks.map((taskGroup, index) => (
+                  <Draggable
+                    draggableId={taskGroup.id}
+                    key={taskGroup.index}
+                    index={index}
+                  >
+                    {(provided) => (
+                      <div
+                        {...provided.dragHandleProps}
+                        {...provided.draggableProps}
+                        ref={provided.innerRef}
+                      >
+                        <div className="step" key={taskGroup.id}>
+                          <h3>{taskGroup.step}</h3>
+                          <div className="taskContainer">
+                            {taskGroup.items.map((task, taskIndex) => (
+                              <div className="task" key={task.id}>
+                                {task.name}
+                              </div>
+                            ))}
+                          </div>
+                        </div>
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+              </div>
+            )}
+          </Droppable>
+          {/* {tasks.map((taskGroup) => (
+            <div className="step" key={taskGroup.id}>
+              <h3>{taskGroup.step}</h3>
+              <div className="taskContainer">
+                {taskGroup.items.map((task, taskIndex) => (
+                  <div className="task" key={task.id}>
+                    {task.name}
+                  </div>
+                ))}
+              </div>
             </div>
-          </div>
-        ))}
+          ))} */}
+        </DragDropContext>
       </div>
     </>
   );
